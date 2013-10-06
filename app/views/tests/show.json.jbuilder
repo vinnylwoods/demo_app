1a8133768eb38d41b549c9b51602e7a862bd1659
vinny <vinnylwoods@gmail.com>
2013-10-06 12:41:17 +1100

    Finish demo app

---
+++ app/views/tests/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @test, :firstname, :lastname, :created_at, :updated_at
---
+++ db/migrate/20131006012121_create_tests.rb
@@ -0,0 +1,10 @@
+class CreateTests < ActiveRecord::Migration
+  def change
+    create_table :tests do |t|
+      t.string :firstname
+      t.string :lastname
+
+      t.timestamps
+    end
+  end
+end
